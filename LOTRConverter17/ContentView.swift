54026afdf6a89d73073438f866331e49dfa5f025
hades unseen <hadesunseenn@gmail.com>
2024-11-08 23:28:12 +0530

    Initial commit

---
+++ LOTRConverter17/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Gurmukh Singh on 2024-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // background image
+            
+            VStack {
+                // Logo
+                
+                // App Text
+                
+                // Currency conversion
+                HStack {
+                    // need 3 columns
+                    
+                    // first column
+                    VStack {
+                        
+                        //currecy
+                        HStack {
+                            //Currecny image
+                            
+                            // Curreny text
+                        }
+                        
+                        //Text Field
+                    }
+                    
+                    // second columns = sign
+                    
+                    
+                    // Right conversion section
+                    VStack {
+                        
+                        //currecy
+                        HStack {
+                            //Currecny text
+                            
+                            // Curreny image
+                        }
+                        
+                        //Text Field
+                    }
+                }
+                
+                //info
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Gurmukh Singh on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
